e4332b3bf2fbf2e0e64d945cdaa4da4fb121c537
taylor.fish <contact@taylor.fish>
2025-06-12 02:24:22 -0700

    benchmark: add license headers

--- benchmark/src/diamond.rs
+++ benchmark/src/diamond.rs
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2025 taylor.fish <contact@taylor.fish>
+ *
+ * This file is part of Eips.
+ *
+ * Eips is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Eips is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with Eips. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use diamond_types::AgentId;
 use diamond_types::list::ListCRDT;
 use diamond_types::list::encoding::EncodeOptions;

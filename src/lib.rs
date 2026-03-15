9a823674d4ff0e7623a7a54d89c0270bc006d61f
taylor.fish <contact@taylor.fish>
2026-03-15 00:47:26 -0700

    Remove unnecessary `feature` attribute

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,6 @@
  */
 
 #![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(feature = "allocator_api", feature(allocator_api))]
 #![cfg_attr(feature = "doc_cfg", feature(doc_cfg))]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![warn(clippy::missing_safety_doc)]
